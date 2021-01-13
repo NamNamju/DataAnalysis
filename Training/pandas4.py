709a2c743c8d36eb47034d5c04ab036b9f258e29
NamNamju <namuju_hi@naver.com>
2021-01-13 23:23:32 +0900

    Pivot_table

---
+++ Training/pandas4.py
@@ -0,0 +1,18 @@
+import numpy as np
+import pandas as pd
+
+df = pd.DataFrame([
+    ['Apple', 7, 5,  'Fruit'],
+    ['Banana', 3, 6, 'Fruit'],
+    ['Coconumt',2, 6, 'Fruit'],
+    ['Rice', 8, 2, 'Meal'],
+    ['Beef', 5,2, 'Meal'],
+    ['Kimchi', 4, 8,'Meal']],
+    columns = ['Name', 'Frequency', 'Importance','Type'])
+
+print(df)
+df = df.pivot_table(
+    index = "Importance", columns="Type",values = "Frequency",
+    aggfunc = np.max
+)
+print(df)
